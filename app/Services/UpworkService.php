7450473053e60eac1c9c547c8164a42b801e0516
Uki7991 <tilek.kubanov@gmail.com>
2021-08-13 17:17:24 +0600

    Update UpworkService.php

--- app/Services/UpworkService.php
+++ app/Services/UpworkService.php
@@ -66,6 +66,7 @@ class UpworkService implements OAuthable
 
     public function getUserInfo()
     {
+        $this->client->auth();
         return (new Users($this->client))->getMyInfo();
     }
 }
