918606eac619f6e72530b369be463e00228a91a2
elijah <hadasevich.e@gmail.com>
2021-12-01 22:04:45 +0600

    elijah: remove dd

--- app/Http/Controllers/Auth/PipedriveController.php
+++ app/Http/Controllers/Auth/PipedriveController.php
@@ -25,7 +25,7 @@ class PipedriveController extends Controller
     }
 
     public function callback(Request $request)
-    {dd($request->code);
+    {
         $userInfo = $this->service->authorize($request->code)->getUserInfo();
 
         abort_if(!$userInfo, 404, 'Whoops, we get an error!');
