f74aed786c947c54776b486af23c9e4aef990347
elijah <hadasevich.e@gmail.com>
2021-12-07 16:14:01 +0600

    elijah: change to user id 5

--- app/Console/Commands/DispatchJob.php
+++ app/Console/Commands/DispatchJob.php
@@ -48,7 +48,7 @@ class DispatchJob extends Command
      */
     public function handle()
     {
-        Auth::login(User::find(2));
+        Auth::login(User::find(5));
 
         $service = new UpworkJobsService();
 
