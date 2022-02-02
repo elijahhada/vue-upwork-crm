faa6f0040b61ddbfd9ed40ce64686190645287d8
elijah <hadasevich.e@gmail.com>
2022-02-02 16:06:50 +0600

    elijah: remove without overlapping

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('get:jobs')->everyMinute()->withoutOverlapping();
-        $schedule->command('move:jobs')->dailyAt('03:00')->timezone('Europe/Moscow')->withoutOverlapping();
+        $schedule->command('move:jobs')->dailyAt('03:00')->timezone('Europe/Moscow');
     }
 
     /**
