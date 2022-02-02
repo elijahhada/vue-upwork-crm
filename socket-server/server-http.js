d3920db10dd4396fd933a9c5ec0c9b1e8559309e
elijah <hadasevich.e@gmail.com>
2022-02-02 14:12:21 +0600

    elijah: try server js with socket io

--- socket-server/server-http.js
+++ socket-server/server-http.js
@@ -1,30 +1,14 @@
-// const HOST = 'https://localhost';
-// const PORT = 3000;
-// const hostname = `${HOST}:${PORT}`;
-//
-// const express = require('express');
-// const app = express();
-// const server = require('http').createServer(app);
-//
-// const io = require('socket.io')(server, {
-//     cors: { origin: '*' },
-// });
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const cors = require('cors')
+const HOST = 'https://localhost';
+const PORT = 3000;
+const hostname = `${HOST}:${PORT}`;
 
-const app = express()
-const server = http.createServer(app)
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+
 const io = require('socket.io')(server, {
     cors: { origin: '*' },
-})
-
-app.use(express.static(path.join(__dirname, 'public')))
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(cors())
-const PORT = 3000 || process.env.PORT
+});
 
 io.on('connection', (socket) => {
     socket.connected && console.log(`→ Socket connected`);
@@ -48,5 +32,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(PORT, () => {
-    console.log(`WS Server running on ${PORT}\n`);
+    console.log(`WS Server running on ${hostname}\n`);
 });
