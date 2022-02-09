bf25f81b67d1e892f4d0db843f06c3465dade255
elijah <hadasevich.e@gmail.com>
2022-02-09 15:31:02 +0600

    elijah: change port from 443 to 445

--- socket-server/server.js
+++ socket-server/server.js
@@ -5,7 +5,8 @@ const http = require('http');
 const https = require('https');
 
 const httpPort = 444;
-const httpsPort = 443;
+const httpsPort = 445;
+
 const httpServer = http.createServer();
 const httpsServer = https.createServer({
     key: readFileSync(join(__dirname, '..', 'ssl', 'key.pem')),
